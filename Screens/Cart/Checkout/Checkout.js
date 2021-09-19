75ddc9c0ef513e3fb4e66b8f061cae09ddfbaecc
vanquy1306 <phamvanquy1306@gmail.com>
2021-09-19 08:08:44 +0700

    User Admin

--- Screens/Cart/Checkout/Checkout.js
+++ Screens/Cart/Checkout/Checkout.js
@@ -5,12 +5,14 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import FormContainer from '../../../Shared/Form/FormContainer'
 import Input from '../../../Shared/Form/Input'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-
+import AuthGlobal from "../../../Context/store/AuthGlobal"
 
 import { connect } from 'react-redux'
 
+const countries = require("../../../assets/countries.json");
+
 const Checkout = (props) => {
-    // const context = useContext(AuthGlobal)
+    const context = useContext(AuthGlobal)
 
     const [ orderItems, setOrderItems ] = useState();
     const [ address, setAddress ] = useState();
@@ -19,22 +21,22 @@ const Checkout = (props) => {
     const [ zip, setZip ] = useState();
     const [ country, setCountry ] = useState();
     const [ phone, setPhone ] = useState();
-    // const [ user, setUser ] = useState();
+    const [ user, setUser ] = useState();
 
     useEffect(() => {
         setOrderItems(props.cartItems)
 
-    //     if(context.stateUser.isAuthenticated) {
-    //         setUser(context.stateUser.user.sub)
-    //     } else {
-    //         props.navigation.navigate("Cart");
-    //         Toast.show({
-    //             topOffset: 60,
-    //             type: "error",
-    //             text1: "Please Login to Checkout",
-    //             text2: ""
-    //         });
-    //     }
+        if(context.stateUser.isAuthenticated) {
+            setUser(context.stateUser.user.sub)
+        } else {
+            props.navigation.navigate("Cart");
+            Toast.show({
+                topOffset: 60,
+                type: "error",
+                text1: "Please Login to Checkout",
+                text2: ""
+            });
+        }
 
         return () => {
             setOrderItems();
